457f4af02ad479aecb5c27736a96b2e40d0aefb2
hjongha <90365579+hjongha@users.noreply.github.com>
2022-11-19 18:22:10 +0900

    Add files via upload

--- main/java/com/example/myapplication/driver/DriverSelect.java
+++ main/java/com/example/myapplication/driver/DriverSelect.java
@@ -56,12 +56,15 @@ public class DriverSelect extends AppCompatActivity {
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH");
     private SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("mm");
     private Date date = new Date();
-    double longitude = 0, latitude = 0;
+    Boolean ApiUse = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_select);
+        ListView list = (ListView) findViewById(R.id.list_bus);
+        EditText edit = (EditText) findViewById(R.id.edit_busnum);
+        Button select_btn = (Button) findViewById(R.id.btn_select);
 
         databaseReference = database.getReference("Driver");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -117,7 +120,7 @@ public class DriverSelect extends AppCompatActivity {
                         dlg_start.setPositiveButton("아니오", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                intent = new Intent(DriverSelect.this, DriverMain.class);
+                                intent = new Intent(DriverSelect.this, DriverMain_Api.class);
                                 startActivity(intent);
                                 finish();
                             }
@@ -126,12 +129,13 @@ public class DriverSelect extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 databaseReference.child(snapshot.getKey()).removeValue();
+                                ApiUse = true;
                             }
                         });
                         dlg_start.setOnCancelListener(new DialogInterface.OnCancelListener() {
                             @Override
                             public void onCancel(DialogInterface dialogInterface) {
-                                intent = new Intent(DriverSelect.this, DriverMain.class);
+                                intent = new Intent(DriverSelect.this, DriverMain_Api.class);
                                 startActivity(intent);
                                 finish();
                             }
@@ -145,155 +149,155 @@ public class DriverSelect extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) { }
         });
         /////////////////////
+        if (!ApiUse) {
+            DriverAdapter adapter = new DriverAdapter();
+            list.setAdapter(adapter);
 
-        ListView list = (ListView) findViewById(R.id.list_bus);
-        DriverAdapter adapter = new DriverAdapter();
-        list.setAdapter(adapter);
+            select_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    input_str = edit.getText().toString();
 
-        EditText edit = (EditText) findViewById(R.id.edit_busnum);
-        Button select_btn = (Button) findViewById(R.id.btn_select);
-        select_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                input_str = edit.getText().toString();
-
-                databaseReference = database.getReference("BusTime").child(input_str);
-                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        busTimeArray.clear();
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            busTimeArray.add(snapshot.getValue(BusTime.class));
-                            adapter.addList(snapshot.getValue(BusTime.class));
+                    databaseReference = database.getReference("BusTime").child(input_str);
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            busTimeArray.clear();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                busTimeArray.add(snapshot.getValue(BusTime.class));
+                                adapter.addList(snapshot.getValue(BusTime.class));
+                            }
+                            adapter.notifyDataSetChanged();
                         }
-                        adapter.notifyDataSetChanged();
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    }
-                });
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        int position = i;
+                        }
+                    });
+
+                    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                            int position = i;
 
 
-                        String time_hours = simpleDateFormat.format(date);
-                        String time_minutes = simpleDateFormat2.format(date);
-                        int n_time = (Integer.parseInt(time_hours)*60) + Integer.parseInt(time_minutes);
-                        int s_time = (busTimeArray.get(position).getHours()*60) + busTimeArray.get(position).getMinutes();
+                            String time_hours = simpleDateFormat.format(date);
+                            String time_minutes = simpleDateFormat2.format(date);
+                            int n_time = (Integer.parseInt(time_hours) * 60) + Integer.parseInt(time_minutes);
+                            int s_time = (busTimeArray.get(position).getHours() * 60) + busTimeArray.get(position).getMinutes();
 
-                        if (n_time < s_time) {
-                            AlertDialog.Builder dlg = new AlertDialog.Builder(DriverSelect.this);
-                            dlg.setTitle("버스 확인");
-                            dlg.setMessage("버스 번호 : " + input_str + "번\n" + "출발 시간 : " + busTimeArray.get(position).getHours() + "시 " + busTimeArray.get(position).getMinutes() + "분\n입력하신 정보가 맞습니까?");
-                            dlg.setPositiveButton("아니오", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    Toast.makeText(DriverSelect.this, "취소하였습니다.", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                            dlg.setNegativeButton("예", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    Toast.makeText(DriverSelect.this, "확인하였습니다.", Toast.LENGTH_SHORT).show();
+                            if (n_time < s_time) {
+                                AlertDialog.Builder dlg = new AlertDialog.Builder(DriverSelect.this);
+                                dlg.setTitle("버스 확인");
+                                dlg.setMessage("버스 번호 : " + input_str + "번\n" + "출발 시간 : " + busTimeArray.get(position).getHours() + "시 " + busTimeArray.get(position).getMinutes() + "분\n입력하신 정보가 맞습니까?");
+                                dlg.setPositiveButton("아니오", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(DriverSelect.this, "취소하였습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                dlg.setNegativeButton("예", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(DriverSelect.this, "확인하였습니다.", Toast.LENGTH_SHORT).show();
 
-                                    databaseReference = database.getReference("Driver");
-                                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                            int j = 0;
-                                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                                j++;
+                                        databaseReference = database.getReference("Driver");
+                                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                int j = 0;
+                                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                                    j++;
+                                                }
+                                                databaseReference.child(Integer.toString(j)).child("Uid").setValue(firebaseUser.getUid());
+                                                databaseReference.child(Integer.toString(j)).child("BusNum").setValue(input_str);
+                                                databaseReference.child(Integer.toString(j)).child("BusTime").setValue(Integer.toString(position + 1));
                                             }
-                                            databaseReference.child(Integer.toString(j)).child("Uid").setValue(firebaseUser.getUid());
-                                            databaseReference.child(Integer.toString(j)).child("BusNum").setValue(input_str);
-                                            databaseReference.child(Integer.toString(j)).child("BusTime").setValue(Integer.toString(position+1));
-                                        }
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) { }
-                                    });
 
-                                    Handler handler = new Handler();
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            intent = new Intent(DriverSelect.this, DriverMain.class);
-                                            startActivity(intent);
-                                            finish();
-                                        }
-                                    }, 1000);
-                                }
-                            });
-                            dlg.show();
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+                                            }
+                                        });
+
+                                        Handler handler = new Handler();
+                                        handler.postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                intent = new Intent(DriverSelect.this, DriverMain.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        }, 1000);
+                                    }
+                                });
+                                dlg.show();
+                            } else {
+                                Toast.makeText(DriverSelect.this, "운행 시간이 지났습니다. 다시 선택해주세요.", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                        else {
-                            Toast.makeText(DriverSelect.this, "운행 시간이 지났습니다. 다시 선택해주세요.", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            });
+        }
+        else {
+            // 기사 - API 버전
+            TextView textView = (TextView) findViewById(R.id.textView9);
+            textView.setText("운행할 지역을 선택해주세요");
+            EditText editText = (EditText) findViewById(R.id.edit_busnum);
+            editText.setHint("지역을 입력하세요");
+
+            String[] api_split = get_api.getCitycode().split("\n");
+            String[] citycodeArr = new String[api_split.length], citynameArr = new String[api_split.length], citynameArr_copy = new String[api_split.length];
+            for (int i = 0; i < api_split.length; i++) {
+                String[] api_split2 = api_split[i].split(" ");
+                citycodeArr[i] = api_split2[0];
+                citynameArr[i] = api_split2[1];
+            }
+            System.arraycopy(citynameArr, 0, citynameArr_copy, 0, api_split.length);
+
+            Arrays.sort(citynameArr_copy);
+            DriverAdapter_api adapter_api = new DriverAdapter_api();
+            list.setAdapter(adapter_api);
+            for (int i = 0; i < citynameArr_copy.length; i++) {
+                adapter_api.addList(citynameArr_copy[i]);
+            }
+            adapter_api.notifyDataSetChanged();
+
+            select_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    input_str = edit.getText().toString();
+                    ArrayList<String> sameCitynameArr = new ArrayList<>();
+                    for (int i = 0; i < citynameArr_copy.length; i++) {
+                        String cityname = adapter_api.getItem(i);
+                        if (cityname.toLowerCase().contains(input_str.toLowerCase())) {
+                            sameCitynameArr.add(cityname);
                         }
                     }
-                });
-            }
-        });
-
-        // 기사 - API 버전
-
-        TextView textView = (TextView) findViewById(R.id.textView9);
-        textView.setText("운행할 지역을 선택해주세요");
-        EditText editText = (EditText) findViewById(R.id.edit_busnum);
-        editText.setHint("지역을 입력하세요");
-
-        String[] api_split = get_api.getCitycode().split("\n");
-        String[] citycodeArr = new String[api_split.length], citynameArr = new String[api_split.length], citynameArr_copy = new String[api_split.length];
-        for (int i = 0; i < api_split.length; i++) {
-            String[] api_split2 = api_split[i].split(" ");
-            citycodeArr[i] = api_split2[0];
-            citynameArr[i] = api_split2[1];
-        }
-        System.arraycopy(citynameArr, 0, citynameArr_copy, 0, api_split.length);
-
-        Arrays.sort(citynameArr_copy);
-        DriverAdapter_api adapter_api = new DriverAdapter_api();
-        list.setAdapter(adapter_api);
-        for (int i=0; i<citynameArr_copy.length; i++) {
-            adapter_api.addList(citynameArr_copy[i]);
-        }
-        adapter_api.notifyDataSetChanged();
-
-        select_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                input_str = edit.getText().toString();
-                ArrayList<String> sameCitynameArr = new ArrayList<>();
-                for (int i=0; i<citynameArr_copy.length; i++) {
-                    String cityname = adapter_api.getItem(i);
-                    if (cityname.toLowerCase().contains(input_str.toLowerCase())) {
-                        sameCitynameArr.add(cityname);
+                    adapter_api.clear();
+                    for (int i = 0; i < sameCitynameArr.size(); i++) {
+                        adapter_api.addList(sameCitynameArr.get(i));
                     }
+                    adapter_api.notifyDataSetChanged();
                 }
-                adapter_api.clear();
-                for (int i=0; i<sameCitynameArr.size(); i++) {
-                    adapter_api.addList(sameCitynameArr.get(i));
-                }
-                adapter_api.notifyDataSetChanged();
-            }
-        });
+            });
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-                Intent intent = new Intent(DriverSelect.this, DriverSelect_api.class);
-                for (int i=0; i<citynameArr.length; i++) {
-                    if (citynameArr[i].equals(adapter_api.getItem(pos))) {
-                        System.out.println("citycode : " + citycodeArr[i] + ", cityname : " + citynameArr[i]);
-                        intent.putExtra("citycode", citycodeArr[i]);
-                        break;
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                    Intent intent = new Intent(DriverSelect.this, DriverSelect_api.class);
+                    for (int i = 0; i < citynameArr.length; i++) {
+                        if (citynameArr[i].equals(adapter_api.getItem(pos))) {
+                            System.out.println("citycode : " + citycodeArr[i] + ", cityname : " + citynameArr[i]);
+                            intent.putExtra("citycode", citycodeArr[i]);
+                            break;
+                        }
                     }
+                    startActivity(intent);
+                    finish();
                 }
-                startActivity(intent);
-                finish();
-            }
-        });
+            });
+        }
     }
 }
